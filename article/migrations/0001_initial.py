97431b6879b760ea3c00680330f8cc7cb9b81b51
Juot <gongpeng9449@gmail.com>
2017-09-27 04:06:29 +0800

    加入天气历史今日

--- article/migrations/0001_initial.py
+++ article/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import models, migrations
 
 
 class Migration(migrations.Migration):
@@ -13,14 +13,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
-                ('category', models.CharField(max_length=50, blank=True)),
+                ('category', models.CharField(blank=True, max_length=50)),
                 ('date_time', models.DateTimeField(auto_now_add=True)),
-                ('content', models.TextField(null=True, blank=True)),
+                ('content', models.TextField(blank=True, null=True)),
             ],
             options={
-                'ordering': ['-date_time'],
             },
+            bases=(models.Model,),
         ),
     ]
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,25 +1,34 @@
-"""my_blog URL Configuration
+# -*- coding: utf-8 -*-
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from article import views
-from django.conf.urls import include
+from article.views import RSSFeed
 
-urlpatterns = [
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home, name = 'home'),
-    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
-]
+    url(r'^$', 'article.views.home', name = 'home'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag/(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    url(r'^search/$','article.views.blog_search', name = 'search'),
+    url(r'^feed/$', RSSFeed(), name = "RSS"),
+)
+
+
+"""
+修改下 index() 视图， 让它显示系统中最新发布的 5 个调查问题，以逗号分割并按发布日期排序：:
+
+from django.http import HttpResponse
+
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+"""
